FelikZ/ctrlp-py-matcher

commit c724ebe5d79da294252b10eb92f426abe041c250

updated the change to do all the escaping in one pass

autoload/pymatcher.py
@@ -78,12 +78,8 @@ def CtrlPPyMatch():
 
     rez.extend([line for score, line in heapq.nlargest(limit, res)])
 
-    # Protect output from '\' (mostly win32 path separators)
-    escrez = [line.replace('\\', '\\\\') for line in rez]
-
     # Use double quoted vim strings
-    vimrez = ['"%s"' % line.replace('"', '\\"') for line in escrez]
+    vimrez = ['"%s"' % line.replace('"', '\\"').replace('\\', '\\\\') for line in rez]
 
     vim.command("let s:regex = '%s'" % regex)
     vim.command('let s:rez = [%s]' % ','.join(vimrez))
-

commit 536f7f1d4c330b66139d075320c615acfa588aa4

Fix string escaping
* Fixes zero matches being reported by CtrlPTag
* Regression introduced in c724ebe5d79da294252b10eb92f426abe041c250

autoload/pymatcher.py
@@ -79,7 +79,7 @@ def CtrlPPyMatch():
     rez.extend([line for score, line in heapq.nlargest(limit, res)])
 
     # Use double quoted vim strings and escape \
-    vimrez = ['"' + line.replace('"', '\\"').replace('\\', '\\\\') + '"' for line in rez]
+    vimrez = ['"' + line.replace('\\', '\\\\').replace('"', '\\"') + '"' for line in rez]
 
     vim.command("let s:regex = '%s'" % regex)
     vim.command('let s:rez = [%s]' % ','.join(vimrez))